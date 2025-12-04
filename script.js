583bd0d16f228a78ffd3e345c84b887584ee6438
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-12-04 18:46:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let count = document.getElementById("counter");
+let increment = document.getElementById("increment");
+function handleClick() {
+  let val = Number(count.innerText);
+  alert(val);
+
+  val++;
+  count.innerText = val;
+}
